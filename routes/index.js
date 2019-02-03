e02cd59a1ed21d7065f3df208dbb838e286ebb44
ashu8912 <ashughildiyal5@gmail.com>
2019-02-03 16:07:00 +0530

    added /stores and /add route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const storeController=require("../controllers/storeController");
+const {catchErrors}=require("../handlers/errorHandlers");
 // Do work here
-router.get('/',storeController.homePage);
-
+router.get('/',catchErrors(storeController.getStores));
+router.get("/stores",catchErrors(storeController.getStores));
+router.get("/add",storeController.addStore);
+router.get("/stores/:id/edit",catchErrors(storeController.editStore))
+router.post("/add",catchErrors(storeController.createStore));
 module.exports = router;
