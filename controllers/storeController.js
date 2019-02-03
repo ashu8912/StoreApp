f2e7a499d34c1e63806e03ebb9413c1f3769015e
ashu8912 <ashughildiyal5@gmail.com>
2019-02-03 16:07:40 +0530

    created methods to handle route operations

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,26 @@
+const mongoose=require("mongoose");
+const Store=mongoose.model("Store");
 exports.homePage=function(req,res){
     res.render("sample",{
         dog:"sandwich"
       });
+}
+exports.createStore=async function(req,res){
+    const store=await (new Store(req.body)).save();
+    req.flash("success","Successfully created Store"); 
+    res.redirect(`/store/${store.slug}`);
+}
+exports.addStore=function(req,res){
+    res.render("editStore",{
+        title:"Add Store"
+    })
+}
+
+exports.getStores=async function(req,res){
+    const stores=await Store.find({});
+    res.render("stores",{title:"Store",stores})
+}
+
+exports.editStore=async function(req,res){
+    
 }
